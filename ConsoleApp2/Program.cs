00bd7a0ca7b581d3d73bc1023f61cad1ff512209
Your Name <you@example.com>
2023-10-06 05:02:27 +0400

    first commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,125 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+       
+            
+            
+            
+        //{
+        //    int balance = 600;
+        //    int button = 1;
+        //    while (button!=0)
+        //    {
+        //        Interface();
+        //        button=int.Parse(Console.ReadLine());
+        //        if (button==1)
+        //        {
+        //            BalansiGoster(balance);                
+        //        }
+        //        if(button==2) 
+        //        {
+        //            Console.WriteLine("Mebleg: ") ;
+        //            int value=int.Parse(Console.ReadLine());
+        //            CashIn(ref balance, value);
+        //        }
+        //        if (button==3)
+        //        {
+        //            Console.WriteLine("Mebleg: "  );
+        //            int value=int.Parse(Console.ReadLine());    
+        //            CashOut(ref balance, value);    
+        //        }
+        //        if (button == 0)
+        //        {
+        //            Console.WriteLine( "Az mebleg, yuksek faiz:Qacmayacaq furset!!!Yararlanmaq ucun telesin." );
+        //            Console.WriteLine(  "Elaqe nom:999");
+        //        }
+        //    }
+        //}
+        //public static void Interface()
+        //{
+        //    Console.WriteLine("Sebuhi banka xos gelmisiz <3");
+        //    Console.WriteLine("==============");
+        //    Console.WriteLine();
+        //    Console.WriteLine("Emeliyyati secin");
+        //    Console.WriteLine("[1] Balansi goster");
+        //    Console.WriteLine("[2] Cash In");
+        //    Console.WriteLine("[3] Cash Out");
+        //    Console.WriteLine("[0] Cix");
+        //    Console.WriteLine();
+        //    Console.WriteLine(">>>>>");
+
+        //}
+        //public static void BalansiGoster(int balance) 
+        //{
+        //    Console.WriteLine("Balans:"+balance);
+        //    Console.WriteLine(  );
+
+        //}
+        //public static void CashOut(ref int balance, int value)
+        //{
+        //    balance -= value;
+        //    Console.WriteLine(  );
+        //    Console.WriteLine("Balans:"+ balance  );
+
+        //}
+        //public static void CashIn(ref int balance, int value)
+        //{
+        //    balance+= value;
+        //    Console.WriteLine(  );
+        //    Console.WriteLine("Balans:" + balance);  
+        //}
+        
+       
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            //Task1
+            //{
+        //    int[] arr = { 1, 2, 3 };
+        //    Console.WriteLine(  );
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        Console.WriteLine( arr[i]);
+        //    }
+        //    Console.WriteLine(  );
+        //    Console.WriteLine("New Array ");
+        //    YeniArray( arr, 4, 6, 8, 10);
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        Console.WriteLine(arr[i]);
+        //    }
+
+        //}
+        //public static void YeniArray(int[] numbers, params int[] Qiymet)
+        //{
+        //    int newLength = numbers.Length + Qiymet.Length;
+        //    int[] NewArray = new int[newLength];
+
+        //    for (int i = 0; i < numbers.Length; i++)
+        //    {
+        //        NewArray[i] = numbers[i];
+        //    }
+        //    for (int i = 0; i < Qiymet.Length; i++)
+        //    {
+        //        NewArray[numbers.Length + i] = Qiymet[i];
+
+        //    }
+        //    numbers = NewArray;
+        //}
+       
+
+        
+    }
+}
